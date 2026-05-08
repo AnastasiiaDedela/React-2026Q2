4f35c90b3b3a2be55daa4c019388f49f0e4e889e
Anastasiia <nastyadedela@gmail.com>
2026-05-08 13:23:17 +0200

    feat: add tests for CardList component

--- src/components/Search/Search.test.tsx
+++ src/components/Search/Search.test.tsx
@@ -3,46 +3,44 @@ import userEvent from '@testing-library/user-event';
 import Search from './Search.tsx';
 import { vi } from 'vitest';
 
-describe('Search', () => {
-  it('renders input and button', () => {
-    render(<Search value="" onChange={vi.fn()} onSearch={vi.fn()} />);
+it('renders input and button', () => {
+  render(<Search value="" onChange={vi.fn()} onSearch={vi.fn()} />);
 
-    expect(screen.getByPlaceholderText(/search pokémon/i)).toBeInTheDocument();
-    expect(screen.getByRole('button')).toBeInTheDocument();
-  });
+  expect(screen.getByPlaceholderText(/search pokémon/i)).toBeInTheDocument();
+  expect(screen.getByRole('button')).toBeInTheDocument();
+});
 
-  it('calls onSearch on button click', async () => {
-    const onSearch = vi.fn();
+it('calls onSearch on button click', async () => {
+  const onSearch = vi.fn();
 
-    render(<Search value="" onChange={vi.fn()} onSearch={onSearch} />);
+  render(<Search value="" onChange={vi.fn()} onSearch={onSearch} />);
 
-    await userEvent.click(screen.getByRole('button'));
+  await userEvent.click(screen.getByRole('button'));
 
-    expect(onSearch).toHaveBeenCalled();
-  });
+  expect(onSearch).toHaveBeenCalled();
+});
 
-  it('calls onSearch when Enter is pressed', async () => {
-    const onSearch = vi.fn();
+it('calls onSearch when Enter is pressed', async () => {
+  const onSearch = vi.fn();
 
-    render(<Search value="pikachu" onChange={vi.fn()} onSearch={onSearch} />);
+  render(<Search value="pikachu" onChange={vi.fn()} onSearch={onSearch} />);
 
-    const input = screen.getByPlaceholderText(/search pokémon/i);
+  const input = screen.getByPlaceholderText(/search pokémon/i);
 
-    await userEvent.click(input);
-    await userEvent.keyboard('{Enter}');
+  await userEvent.click(input);
+  await userEvent.keyboard('{Enter}');
 
-    expect(onSearch).toHaveBeenCalled();
-  });
+  expect(onSearch).toHaveBeenCalled();
+});
 
-  it('calls onChange when typing', async () => {
-    const onChange = vi.fn();
+it('calls onChange when typing', async () => {
+  const onChange = vi.fn();
 
-    render(<Search value="" onChange={onChange} onSearch={vi.fn()} />);
+  render(<Search value="" onChange={onChange} onSearch={vi.fn()} />);
 
-    const input = screen.getByPlaceholderText(/search pokémon/i);
+  const input = screen.getByPlaceholderText(/search pokémon/i);
 
-    await userEvent.type(input, 'pika');
+  await userEvent.type(input, 'pika');
 
-    expect(onChange).toHaveBeenCalled();
-  });
+  expect(onChange).toHaveBeenCalled();
 });
